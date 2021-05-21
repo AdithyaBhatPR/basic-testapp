b745ddaccfdfdd582934f151918c9640f4c6f32e
AdithyaBhatPR <adithyabhatpr@gmail.com>
2021-05-21 20:03:40 +0530

    Added a countdown for the app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const launchtime = new Date('05/30/2021 10:00').getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const millisec_rem = parseInt(launchtime - currentTime)
+  res.render('index', { title: 'My Cool App' , countdown: millisec_rem});
 });
 
 module.exports = router;
